c00f90a7516ebfd96da88fe5bbcc48fec38acd8c
Alex Bennett <benedictus.ab@gmail.com>
2019-06-24 18:17:35 -0600

    main screen layout

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, {Component, Fragment} from 'react';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default class App extends Component {
+  state = { artist: '', title: '' }
   render() {
+    const { artist, title } = this.state
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.welcome}>To get started, edit App.js</Text>
+        <Text style={styles.header}>Lyric-ify</Text>
+        <Text style={styles.label}>Artist</Text>
+        <TextInput
+          value={artist}
+          style={styles.input}
+          onChangeText={ (text) => this.setState({ artist: text }) }
+        />
+        <Text style={styles.label}>Song Title</Text>
+        <TextInput
+          value={title}
+          style={styles.input}
+          onChangeText={ (text) => this.setState({ title: text }) }
+        />
       </View>
     );
   }
@@ -15,13 +29,23 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-start',
+    paddingTop: 80,
+    backgroundColor: '#50C878',
   },
-  welcome: {
-    fontSize: 20,
+  label: {
+    marginLeft: 5,
+  },
+  input: {
+    height: 50,
+    fontSize: 25,
+    borderColor: 'grey',
+    borderWidth: 1,
+    margin: 5,
+    backgroundColor: '#fff',
+  },
+  header: {
+    fontSize: 50,
     textAlign: 'center',
-    margin: 10,
   },
 });
